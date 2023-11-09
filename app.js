a346212b94815c99254b270684e1dfbc8c265b08
Quynh Ngan Dao <nguy2884@umn.edu>
2023-11-09 12:11:23 -0600

    generic interface: function type

--- app.js
+++ app.js
@@ -10,3 +10,12 @@ var c = {
 };
 console.log(c.id);
 console.log(c.run());
+function checkElementAt(items, toBeChecked, atIndex) {
+    return items[atIndex] == toBeChecked;
+}
+var checker = checkElementAt;
+var items = [1, 3, 5, 7];
+var b = checker(items, 5, 1);
+console.log(b);
+var b2 = checker(items, 5, 2);
+console.log(b2);
--- app.ts
+++ app.ts
@@ -24,3 +24,24 @@ let c: Command<string, number> = {
 
 console.log(c.id);
 console.log(c.run());
+
+
+// interfaces describing function type 
+interface ElementChecker {
+    <T>(items: T[], toBeChecked: T, atIndex: number): boolean;
+}
+
+function checkElementAt<T>(
+    items: T[],
+    toBeChecked: T,
+    atIndex: number 
+    ) : boolean {
+        return items[atIndex] == toBeChecked;
+    }
+
+    let checker: ElementChecker = checkElementAt;
+    let items = [1,3,5,7];
+    let b: boolean = checker<number>(items, 5,1);
+    console.log(b); // false, at index 1, it is 3 not 5
+    let b2: boolean = checker<number>(items, 5,2);
+    console.log(b2); // true , at index of 2, it is 5
