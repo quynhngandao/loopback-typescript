88da19a3c1b5ea24e635a59751f3051998c8d36a
Quynh Ngan Dao <nguy2884@umn.edu>
2023-11-09 13:17:06 -0600

    generic classes and interfaces

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-function LoggerAndReturn(thing) {
-    return thing;
-}
-// const message: string = LoggerAndReturn<string>('Hello world');
-var message = LoggerAndReturn('Hello world');
-var message2 = LoggerAndReturn(2);
-console.log(message);
-console.log(message2);
+var GenericNumber = /** @class */ (function () {
+    function GenericNumber() {
+    }
+    return GenericNumber;
+}());
+var myGenericNumber = new GenericNumber();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function (x, y) {
+    return x + y;
+};
+var List = /** @class */ (function () {
+    function List() {
+        this.data = [];
+    }
+    List.prototype.add = function (t) {
+        this.data.push(t);
+    };
+    List.prototype.remove = function (t) {
+        var index = this.data.indexOf(t);
+        if (index > -1) {
+            this.data.splice(index, 1);
+        }
+    };
+    List.prototype.asArray = function () {
+        return this.data;
+    };
+    return List;
+}());
+var numbers = new List();
+numbers.add(11);
+numbers.add(12);
+numbers.add(13);
+numbers.remove(12);
+var numArray = numbers.asArray();
+console.log(numArray);
--- app.ts
+++ app.ts
@@ -1,9 +1,49 @@
-function LoggerAndReturn<T>(thing: T) : T {
-    return thing;
+// Generic class 'GenericNumber' with a generic type 'T'
+class GenericNumber<T> {
+  zeroValue: T;
+  add: (x: T, y: T) => T;
 }
 
-// const message: string = LoggerAndReturn<string>('Hello world');
-const message: string = LoggerAndReturn('Hello world');
-const message2: number = LoggerAndReturn(2)
-console.log(message);
-console.log(message2);
+// Creating an instance of 'GenericNumber' with type 'number'
+let myGenericNumber = new GenericNumber<number>();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function (x, y) {
+  return x + y;
+};
+
+// Generic interface 'Collection' with a generic type 'T'
+interface Collection<T> {
+  add(t: T): void;
+  remove(t: T): void;
+  asArray(): T[];
+}
+
+// Implementation of the 'Collection' interface in the 'List' class
+class List<T> implements Collection<T> {
+  private data: T[] = [];
+
+  add(t: T): void {
+    this.data.push(t);
+  }
+
+  remove(t: T): void {
+    let index = this.data.indexOf(t);
+    if (index > -1) {
+      this.data.splice(index, 1);
+    }
+  }
+  asArray(): T[] {
+    return this.data;
+  }
+}
+
+// Creating an instance of 'List' with type 'number'
+let numbers: Collection<number> = new List();
+numbers.add(11);
+numbers.add(12);
+numbers.add(13);
+numbers.remove(12);
+let numArray = numbers.asArray();
+
+// Outputting the resulting array
+console.log(numArray);
