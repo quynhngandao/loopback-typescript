4ca3f5a86ddb3e1323e2326a95612d4a65a72789
Quynh Ngan Dao <nguy2884@umn.edu>
2023-11-09 01:06:08 -0600

    generic function: T array type

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-function LoggerAndReturn(thing) {
-    return thing;
+function getArray(items) {
+    return new Array().concat(items);
 }
-// const message: string = LoggerAndReturn<string>('Hello world');
-var message = LoggerAndReturn('Hello world');
-var message2 = LoggerAndReturn(2);
-console.log(message);
-console.log(message2);
+// let myNumArr = getArray<number>([100,200,300]);
+// let myStArr = getArray<string>(['Hello', 'World']);
+var myNumArr = getArray([100, 200, 300]);
+var myStArr = getArray(['Hello', 'World']);
+console.log(myNumArr);
+console.log(myStArr);
+myNumArr.push('Hello');
+myStArr.push(1);
--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
-function LoggerAndReturn<T>(thing: T) : T {
-    return thing;
+function getArray<T>(items: T[]) : T[] {
+    return new Array<T>().concat(items);
 }
 
-// const message: string = LoggerAndReturn<string>('Hello world');
-const message: string = LoggerAndReturn('Hello world');
-const message2: number = LoggerAndReturn(2)
-console.log(message);
-console.log(message2);
+// let myNumArr = getArray<number>([100,200,300]);
+// let myStArr = getArray<string>(['Hello', 'World']);
+let myNumArr = getArray([100,200,300]);
+let myStArr = getArray(['Hello', 'World']);
+
+console.log(myNumArr);
+console.log(myStArr);
+// myNumArr.push('Hello');  // compiler error
+// myStArr.push(1);
